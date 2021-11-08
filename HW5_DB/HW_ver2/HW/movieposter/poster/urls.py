f84cc4e4e7618f2451bf11daab96156a1be7a50c
grmtveeen <grmtveeen@gmail.com>
2021-11-08 19:33:03 +0300

    Вообще все работает включая методы delete/create

--- HW5_DB/HW_ver2/HW/movieposter/poster/urls.py
+++ HW5_DB/HW_ver2/HW/movieposter/poster/urls.py
@@ -5,6 +5,6 @@ from .views import posters_list, poster_description, create_poster, delete_poste
 urlpatterns = {
     path('', posters_list, name='posters_list'),                                    #render and list
     path('create/', create_poster, name='create_poster'),                           #create
-    path('delete/', delete_poster, name='delete_poster'),                           #delete
+    path('delete/<int:movie_id>/', delete_poster, name='delete_poster'),            #delete
     path('<int:post_id>/', poster_description, name='poster_description'),          #description
 }
--- HW5_DB/HW_ver2/HW/movieposter/poster/views.py
+++ HW5_DB/HW_ver2/HW/movieposter/poster/views.py
@@ -6,17 +6,6 @@ from genres.models import Genre
 from movies.models import Movie
 import json
 
-posters = [dict(id=1, name='Большой лебовский', description='Джефф Лебовски по прозвищу Чувак — самый ленивый человек в Лос-Анджелесе: закоренелый холостяк, которого ничего не интересует, кроме боулинга и алкоголя. Но однажды к Чуваку врываются два бандита и начинают пугать долгом в миллион долларов.', main_actor='Джефф Бриджес', year=2021,
-              link="https://www.youtube.com/embed/M6_JJK5IIDU", date='18:46', img='static/img/header5.jpg'),
-         dict(id=2, name='Гнев человеческий', description='В инкассаторскую компанию устраивается новый сотрудник Эйч (Джейсон Стэйтем). Он тих и немногословен, что, в принципе, приветствуется в такой сфере, где каждый день перевозят по Лос-Анджелесу миллионы долларов наличными. Но внутри этого загадочного британца бушуют страсти. Он жаждет отомстить банде грабителей, нападавших на инкассаторские машины. Это история мести, которая перемещается по временной шкале то в прошлое, то в настоящее, а также показанная с позиции разных персонажей.', main_actor='Джейсон Стэтхем', year=2021,
-              date='15:35', link="https://www.youtube.com/embed/YLw55x-zOSo", img='static/img/header1.jpg'),
-         dict(id=3, name='Интерстеллар', description='Братья Кристофер и Джонатан Нолан представляют космическую одиссею под названием «Интерстеллар».Фантастическая драма, в главных ролях которой снялись Мэттью МакКонахии, Энн Хэтэуэй и Джессика Честейн, была номинирована на множество премий и получила «Оскар» за лучшие визуальные эффекты. Действие переносит зрителя в недалекое будущее, когда космос уже почти освоен шагнувшим вперед человечеством, но ресурсы Земли истощены. Из-за растущей концентрации азота на нашей планете царит засуха и, как следствие, глобальный продовольственный кризис. Сотрудники секретной лаборатории НАСА обнаруживают в районе орбиты Сатурна червоточину, позволяющую преодолеть пространственно-временной барьер и в кратчайшие сроки переместиться в другую галактику. Возможно, это шанс на спасение для всего человечества. В рамках программы «Лазарь» совершается первая экспедиция, в результате которой по ту сторону так называемой кротовой норы обнаруживаются три наиболее подходящих для жизни планеты. Теперь настало время для второй экспедиции: профессору Бренду и его коллегам предстоит, рискуя собственными жизнями, провести исследования в условиях, когда один час равен нескольким годам на Земле, и найти способ переселения землян на другую планету. Смогут ли ученые спасти человечество, можно узнать, если смотреть онлайн «Интерстеллар».', main_actor='Мэттью Макконехи', year=2014, date='12:20',
-              link="https://www.youtube.com/embed/qcPfI0y7wRU", img='static/img/header2.jpg'),
-         dict(id=4, name='Игра в кальмара', description='Из-за нужды в деньгах сотни игроков принимают странное приглашение поучаствовать в детских соревнованиях. Их ждет заманчивый приз, но ставки смертельно высоки.', main_actor='Ли Джон Джэ', year=2021, date='12:20',
-              link="https://www.youtube.com/embed/6sI7pZuQZPg", img='static/img/header3.jpg'),
-         dict(id=5, name='Черная Вдова', description='Скарлетт Йоханссон в блокбастере Marvel, посвященном одной из «‎Мстителей»‎ Наташе Романофф. Наташа, она же ‎Черная Вдова‎ — бывшая сотрудница КГБ, прошлое которой хранит множество загадок. Сейчас ей предстоит столкнуться со всеми мрачными тайнами лицом к лицу. Пора узнать, как Наташа стала Черной Вдовой.', main_actor='Скарлетт Йоханссон', year=2021, date='12:20',
-              link="https://www.youtube.com/embed/0k82-pGgryk", img='static/img/header4.jpg')]
-
 
 def poster_description(request, post_id):
     if request.method == 'GET':
@@ -24,7 +13,7 @@ def poster_description(request, post_id):
             Movie.objects.get(id=post_id)
         except Movie.DoesNotExist:
             raise Http404
-        return render(request, 'poster/poster_description.html', context={'posters': Movie.objects.get(id=post_id)})
+        return render(request, 'poster/poster_description.html', context={'movie': Movie.objects.get(id=post_id)})
     return JsonResponse({'ER': 'You can use only GET for that'}, status=405)
 
 
@@ -40,40 +29,42 @@ def posters_list(request):
 def create_poster(request):
     if request.method == 'POST':
         request_decode = json.loads(request.body.decode('utf-8'))
-        if 'name' in request_decode:
+        if 'actor_name' in request_decode:
             try:
-                Actor.objects.get(name=request_decode['name'])
+                Actor.objects.get(name=request_decode['actor_name'])
             except Actor.DoesNotExist:
-                Actor.objects.create(name=request_decode['name'], born=request_decode['born'],
-                                     description=request_decode['description'])
-        elif 'date' in request_decode:
+                Actor.objects.create(name=request_decode['actor_name'], born=request_decode['born'],
+                                     description=request_decode['description_actor'])
+        if 'date' in request_decode:
             try:
                 Poster.objects.get(date=request_decode['date'])
             except Poster.DoesNotExist:
                 Poster.objects.create(date=request_decode['date'])
-        elif 'genre' in request_decode:
+        if 'genre' in request_decode:
             try:
                 Genre.objects.get(genre=request_decode['genre'])
             except Genre.DoesNotExist:
                 Genre.objects.create(genre=request_decode['genre'])
-        elif 'title' in request_decode:
+        if 'title' in request_decode:
             Movie.objects.create(title=request_decode['title'],
-                                 main_actor_id=Actor.objects.get(name=request_decode['main_actor']).id,
+                                 main_actor_id=Actor.objects.get(name=request_decode['actor_name']).id,
                                  genre_id=Genre.objects.get(genre=request_decode['genre']).id,
                                  year=request_decode['year'], link=request_decode['link'], img=request_decode['img'],
-                                 description=request_decode['description'],
+                                 description=request_decode['description_movie'],
                                  poster_id=Poster.objects.get(date=request_decode['date']).id)
-        else:
+        if 'actor_name' not in request_decode and 'date' not in request_decode and 'genre' not in request_decode and \
+                'title' not in request_decode:
             return JsonResponse({'ER': 'JSON request does not match the format!'}, status=400)
         return JsonResponse({'OK': 'created!'}, status=200)
     return JsonResponse({'ER': 'You have to use only POST for that'}, status=405)
 
 
-def delete_poster(request):
+def delete_poster(request, movie_id):
     if request.method == 'GET':
-        if len(posters) > 0:
-            posters.pop()
-            return JsonResponse({'OK': 'last deleted!'}, status=200)
-        else:
-            return JsonResponse({'ER': 'Poster is empty'}, status=412)
-    return JsonResponse({'ER': 'You have to use only POST for that'}, status=405)
+        try:
+            p = Movie.objects.get(id=movie_id)
+        except Genre.DoesNotExist:
+            return JsonResponse({'ER': 'Movie DoesNotExist'}, status=412)
+        p.delete()
+        return JsonResponse({'OK': 'Movie deleted!'}, status=200)
+    return JsonResponse({'ER': 'You have to use only GET for that'}, status=405)
